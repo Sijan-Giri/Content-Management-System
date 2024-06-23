4ebb230f9ae7ebd925d2a26f01fe73c0566f2997
Sijan-Giri <girisijan346@gmail.com>
2024-06-23 19:28:24 +0545

    Implementing create blog

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const { connectDatabase } = require('./database/database');
+const Blog = require('./model/blogModel');
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 connectDatabase();
 
@@ -12,9 +16,19 @@ app.get("/",(req,res) => {
     })
 })
 
-app.get('/contact' , (req,res) => {
-    res.send("hi iam contact");
+app.post("/createBlog",(req,res) => {
+    const {title,subTitle,description} = req.body;
+    Blog.create({
+        title,
+        subTitle,
+        description
+    })
+    res.json({
+        status :200,
+        message : "yoou are in create blog page"
+    })
 })
+
 
 app.listen(3000,() => {
     console.log("Server is starting...");
---
+++ model/blogModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogModel = new Schema({
+    title : {
+        type : String
+    },
+    subTitle : {
+        type : String
+    },
+    description : {
+        type : String
+    }
+},{
+    timestamps : true
+})
+
+const Blog = mongoose.model("Blog",blogModel);
+module.exports = Blog;
