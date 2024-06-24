2bbcbdb9104d41c62c1ad7a4aeeaa59facd3f31c
Sijan-Giri <girisijan346@gmail.com>
2024-06-24 16:38:06 +0545

    Implementing fetch blog

--- app.js
+++ app.js
@@ -8,27 +8,48 @@ app.use(express.urlencoded({extended : true}));
 
 connectDatabase();
 
-app.get("/",(req,res) => {
-    res.json({
-        message : "iam from home page",
-        status : 200,
-        id : "09348754389"
-    })
+app.get("/blogs",async (req,res) => {
+    const blog = await Blog.find();
+    if(blog.length > 0) {
+        res.status(200).json({
+            message : "Blog fetched successfully",
+            data : blog
+        })
+    }
+    else {
+        res.status(404).json({
+            message : "No blogs found"
+        })
+    }
 })
 
-app.post("/createBlog",(req,res) => {
+app.get("/blogs/:id",async (req,res) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    if(blog) {
+        res.status(200).json({
+            message : "Blog fetched successfully",
+            data : blog
+        })
+    }
+    else {
+        res.status(404).json({
+            message : "No Blog found for this id"
+        })
+    }
+})
+
+app.post("/createBlog", async (req,res) => {
     const {title,subTitle,description} = req.body;
-    Blog.create({
+    await Blog.create({
         title,
         subTitle,
         description
     })
-    res.json({
-        status :200,
+    res.status(200).json({
         message : "yoou are in create blog page"
     })
 })
-
 
 app.listen(3000,() => {
     console.log("Server is starting...");
--- database/database.js
+++ database/database.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 
 exports.connectDatabase = async () => {
     await mongoose.connect("mongodb+srv://girisijan346:sijan@cluster0.0uczuzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("Database connected successfully")
 }
